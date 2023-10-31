8f6c5903eae2e4611a9121c625cd86bc840a1c2a
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 11:18:03 +0330

    Discussing MedalRepository

--- SamShop.Core/Interfaces/IRepository/IMedalRepository.cs
+++ SamShop.Core/Interfaces/IRepository/IMedalRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SamShop.Domain.Core.Models.Entity;
 
 namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface IMedalRepository
     {
+        IEnumerable<Medal> GetAllMedal();
+        Task<Medal?> GetMedalById(int id);
+        Task AddMedal(Medal Medal);
+        Task UpdateMedal(Medal Medal);
+        Task DeleteMedal(int id);
     }
 }
