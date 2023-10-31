b8ef2bbcdad76da06cdb675908547de9e581e892
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 10:42:55 +0330

    Discussing CartRepository

--- SamShop.Core/Interfaces/IRepository/ICartRepository.cs
+++ SamShop.Core/Interfaces/IRepository/ICartRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamShop.Domain.Core.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface ICartRepository
     {
+        IEnumerable<Cart> GetAllCart();
+        Task<Cart?> GetCartById(int id);
+        Task AddCart(Cart Cart);
+        Task UpdateCart(Cart Cart);
+        Task DeleteCart(int id);
     }
 }
--- SamShop.DataAccess/Repository/CartRepository.cs
+++ SamShop.DataAccess/Repository/CartRepository.cs
@@ -3,11 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SamShop.Application.EntityFramework.DBContext;
 using SamShop.Domain.Core.Interfaces.IRepository;
+using SamShop.Domain.Core.Models.Entity;
 
 namespace SamShop.Application.DataAccess.Repository
 {
     internal class CartRepository : ICartRepository
     {
+        protected readonly SamShopDbContext _context;
+
+        public CartRepository(SamShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddCart(Cart Cart)
+
+        {
+            Cart CartAdding = new Cart()
+            {
+                TotalPrice = Cart.TotalPrice,
+                IsCanceled = false,
+                IsPayed = false
+
+            };
+            await _context.Carts.AddAsync(CartAdding);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Cart> GetAllCart()
+        {
+            return _context.Carts;
+        }
+
+
+
+        public async Task<Cart?> GetCartById(int id)
+        {
+            return await _context.Carts.FirstOrDefaultAsync(c => c.CartId == id);
+
+        }
+        public async Task UpdateCart(Cart Cart)
+        {
+            Cart? changeCart = await _context.Carts.FirstOrDefaultAsync(c => c.CartId == Cart.CartId);
+            if (changeCart != null)
+            {
+                changeCart.TotalPrice = Cart.TotalPrice;
+                changeCart.IsCanceled = Cart.IsCanceled;
+                changeCart.IsPayed = Cart.IsPayed;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteCart(int id)
+        {
+            Cart? removingaCart = await _context.Carts.FirstOrDefaultAsync(c => c.CartId == id);
+            if (removingaCart != null)
+            {
+                _context.Remove(removingaCart);
+            }
+            await _context.SaveChangesAsync();
+        }
     }
 }
