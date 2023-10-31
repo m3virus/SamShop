523c83183be7271a65135b9d5de995f97d47a52c
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 11:28:55 +0330

    Discussing ProductRepository

--- SamShop.Core/Interfaces/IRepository/IProductRepository.cs
+++ SamShop.Core/Interfaces/IRepository/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamShop.Domain.Core.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface IProductRepository
     {
+        IEnumerable<Product> GetAllProduct();
+        Task<Product?> GetProductById(int id);
+        Task AddProduct(Product Product);
+        Task UpdateProduct(Product Product);
+        Task DeleteProduct(int id);
     }
 }
