5e36f405016523320f2639e7efca2db5de0d805c
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 11:32:47 +0330

    discuss sellerRepository

--- SamShop.Core/Interfaces/IRepository/ISellerRepository.cs
+++ SamShop.Core/Interfaces/IRepository/ISellerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamShop.Domain.Core.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface ISellerRepository
     {
+        IEnumerable<Seller> GetAllSeller();
+        Task<Seller?> GetSellerById(int id);
+        Task AddSeller(Seller Seller);
+        Task UpdateSeller(Seller Seller);
+        Task DeleteSeller(int id);
     }
 }
--- SamShop.DataAccess/Repository/SellerRepository.cs
+++ SamShop.DataAccess/Repository/SellerRepository.cs
@@ -3,11 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SamShop.Application.EntityFramework.DBContext;
 using SamShop.Domain.Core.Interfaces.IRepository;
+using SamShop.Domain.Core.Models.Entity;
 
 namespace SamShop.Application.DataAccess.Repository
 {
     internal class SellerRepository:ISellerRepository
     {
+        protected readonly SamShopDbContext _context;
+
+        public SellerRepository(SamShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSeller(Seller Seller)
+
+        {
+            Seller SellerAdding = new Seller()
+            {
+                UserName = Seller.UserName,
+                FirstName = Seller.FirstName,
+                LastName = Seller.LastName,
+                Phone = Seller.Phone,
+                Email = Seller.Email,
+                Wallet = Seller.Wallet,
+                Password = Seller.Password,
+
+            };
+            await _context.Sellers.AddAsync(SellerAdding);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Seller> GetAllSeller()
+        {
+            return _context.Sellers;
+        }
+
+
+
+        public async Task<Seller?> GetSellerById(int id)
+        {
+            return await _context.Sellers.FirstOrDefaultAsync(s => s.SellerId == id);
+
+        }
+
+        public async Task UpdateSeller(Seller Seller)
+        {
+            Seller? changeSeller =
+                await _context.Sellers.FirstOrDefaultAsync(s => s.SellerId == Seller.SellerId);
+            if (changeSeller != null)
+            {
+                changeSeller.UserName = Seller.UserName;
+                changeSeller.FirstName = Seller.FirstName;
+                changeSeller.LastName = Seller.LastName;
+                changeSeller.Wallet = Seller.Wallet;
+                changeSeller.Password = Seller.Password;
+                changeSeller.Email = Seller.Email;
+                changeSeller.Phone = Seller.Phone;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteSeller(int id)
+        {
+            Seller? removingaSeller = await _context.Sellers.FirstOrDefaultAsync(s => s.SellerId == id);
+            if (removingaSeller != null)
+            {
+                _context.Remove(removingaSeller);
+            }
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
