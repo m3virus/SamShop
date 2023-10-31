c7eeb26e76822c8bd35c5cf66c88c6f27f40cd30
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 10:32:21 +0330

    Duscussing AuctionRepository

--- SamShop.Core/Interfaces/IRepository/IAuctionRepository.cs
+++ SamShop.Core/Interfaces/IRepository/IAuctionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamShop.Domain.Core.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface IAuctionRepository
     {
+        IEnumerable<Auction> GetAllAuction();
+        Task<Auction?> GetAuctionById(int id);
+        Task AddAuction(Auction Auction);
+        Task UpdateAuction(Auction Auction);
+        Task DeleteAuction(int id);
     }
 }
