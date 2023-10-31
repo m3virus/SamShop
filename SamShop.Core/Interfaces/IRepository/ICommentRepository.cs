3c7f26dbc99a8ab67a04e0020410d486acedb0fb
mohammad_hasan <mryazdani98@outlook.com>
2023-10-31 11:02:00 +0330

    Discussing CommentRepository

--- SamShop.Core/Interfaces/IRepository/ICommentRepository.cs
+++ SamShop.Core/Interfaces/IRepository/ICommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamShop.Domain.Core.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SamShop.Domain.Core.Interfaces.IRepository
 {
     public interface ICommentRepository
     {
+        IEnumerable<Comment> GetAllComment();
+        Task<Comment?> GetCommentById(int id);
+        Task AddComment(Comment Comment);
+        Task UpdateComment(Comment Comment);
+        Task DeleteComment(int id);
     }
 }
