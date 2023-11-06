211256b5868a99d45ad6c4786cbaee8f25e71115
mohammad_hasan <mryazdani98@outlook.com>
2023-11-06 06:23:30 +0330

    adding Identity

--- SamShop.Application.EntityFramework/DBContext/SamShopDbContext.cs
+++ SamShop.Application.EntityFramework/DBContext/SamShopDbContext.cs
@@ -49,7 +49,7 @@ public partial class SamShopDbContext : IdentityDbContext<IdentityUser, Identity
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=SamShopDB1;integrated security=sspi;TrustServerCertificate=True;MultipleActiveResultSets=true");
+        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=SamShopDB2;integrated security=sspi;TrustServerCertificate=True;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -347,13 +347,11 @@ public partial class SamShopDbContext : IdentityDbContext<IdentityUser, Identity
             entity.HasData(GetSellers());
         });
 
-        OnModelCreatingPartial(modelBuilder);
+        base.OnModelCreating(modelBuilder);
     }
 
     #endregion
 
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
     #region Dataseeder
 
